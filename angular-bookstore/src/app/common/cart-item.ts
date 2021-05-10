d356f25baa6179f8d5c9115d538584fbeb4890f6
phenphak <phenphak.tae86@gmail.com>
2021-05-10 14:45:53 +0700

    Completed the feature 9

--- angular-bookstore/src/app/common/cart-item.ts
+++ angular-bookstore/src/app/common/cart-item.ts
@@ -2,16 +2,16 @@ import { Book } from "./book";
 
 export class CartItem {
   id: string;
-    name: string;
-    imageUrl: string;
-    unitPrice: number;
-    quantity: number;
+  name: string;
+  imageUrl: string;
+  unitPrice: number;
+  quantity: number;
 
-    constructor(book: Book){
-        this.id = book.id;
-        this.name = book.name;
-        this.imageUrl = book.imageUrl;
-        this.unitPrice = book.unitPrice;
-        this.quantity = 1
-    }
+  constructor(book: Book) {
+    this.id = book.id;
+    this.name = book.name;
+    this.imageUrl = book.imageUrl;
+    this.unitPrice = book.unitPrice;
+    this.quantity = 1
+  }
 }
---
+++ angular-bookstore/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems : CartItem[]=[];
+  totalPrice :number=0;
+  totalQuantity :number=0;
+  constructor(private cartService:CartService) { }
+
+  ngOnInit(): void {
+    this.carDetails();
+  }
+  carDetails() {
+    this.cartItems=this.cartService.cartItems;
+
+    //subscribe to the events
+    this.cartService.totalPrice.subscribe(
+      data =>this.totalPrice =data
+    );
+    this.cartService.totalQuantity.subscribe(
+      data =>this.totalQuantity =data
+    );
+    this.cartService.calculateTotalPrice();
+  }
+
+}
